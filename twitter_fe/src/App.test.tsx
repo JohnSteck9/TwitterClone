fb01238fb6f62ce77cc6154c89e5466f2def2f66
John Steck <m0nitor2048@gmail.com>
2023-01-11 23:32:50 +0200

    fix test

--- twitter_fe/src/App.test.tsx
+++ twitter_fe/src/App.test.tsx
@@ -3,8 +3,8 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import './i18n';
 
-test('renders learn react link', () => {
+test('renders react app', () => {
     render(<App />);
-    const linkElement = screen.getByText('English');
+    const linkElement = screen.getByText('App');
     expect(linkElement).toBeInTheDocument();
 });
