fbb2d4b9b0b3ce4962e4a8d341831f922f8a924c
John Steck <m0nitor2048@gmail.com>
2023-01-12 02:43:41 +0200

    test routing

--- twitter_fe/src/pages/ErrorPage/ErrorPage.tsx
+++ twitter_fe/src/pages/ErrorPage/ErrorPage.tsx
@@ -6,6 +6,8 @@ const ErrorPage: FC = () => {
         <>
             <div>Error Page 404</div>
             <Link to="/">APP</Link>
+            <Link to="/sign-in">SIGN-IN</Link>
+            <Link to="/sign-up">SIGN-UP</Link>
         </>
     );
 };
